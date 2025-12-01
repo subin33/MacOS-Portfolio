14e014705bd68b93a196505cd682869f12677661
subin33 <shtnqls33@gmail.com>
2025-12-01 10:26:23 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <>
+      <div>
+        <h1 class="text-3xl font-bold underline">Welcome to my MacFolio</h1>
+      </div>
+    </>
+  );
+};
+
+export default App;
