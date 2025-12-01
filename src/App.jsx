01d03b6f1f3bd2486b67bfe2ff7e1459d5166e46
subin33 <shtnqls33@gmail.com>
2025-12-01 11:25:57 +0900

    feat: implement Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import React from "react";
+import Navbar from "#components/Navbar";
+
 const App = () => {
   return (
-    <>
-      <div>
-        <h1 class="text-3xl font-bold underline">Welcome to my MacFolio</h1>
-      </div>
-    </>
+    <main>
+      <Navbar />
+    </main>
   );
 };
 
