7d18be9958b09588fa08da8b1ce1b83f419ffbd9
subin33 <shtnqls33@gmail.com>
2025-12-02 18:37:16 +0900

    feat: add initial Dock component with icons and tooltip support

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 import Navbar from "#components/Navbar.jsx";
 import Welcome from "#components/Welcome.jsx";
+import Dock from "./Dock copy";
 
-export { Navbar, Welcome };
+export { Navbar, Welcome, Dock };
